cb56a55acab52cbd92914471b18c8464a09f4270
Anders Wind <awia00@gmail.com>
2018-05-17 15:07:33 +0200

    Updated to webpack 4 and generally updated packages

--- Site/webpack.config.js
+++ Site/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const ENV = process.env.npm_lifecycle_event;
@@ -38,11 +38,11 @@ module.exports = {
                 loader: 'tslint-loader',
             },
             {
-                test: /\.scss$/,
-                loader: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: 'css-loader!sass-loader',
-                }),
+                test: /\.s?[ac]ss$/,
+                use: [!isProd ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
             },
         ],
     },
@@ -55,8 +55,11 @@ module.exports = {
             chunksSortMode: 'dependency',
         }),
 
-        new ExtractTextPlugin({
-            filename: 'css/[name].[hash].css'
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: !isProd ? '[name].css' : '[name].[hash].css',
+            chunkFilename: !isProd ? '[id].css' : '[id].[hash].css',
         }),
 
         new CopyWebpackPlugin([{ from: 'public' }]),
